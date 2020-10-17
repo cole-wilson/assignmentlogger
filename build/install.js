d1b4313b81c76d74a3d317293afa255a23b5e4fd
cwi <GeekMonster842@gmail.com>
2020-10-17 01:58:53 +0000

    a2hs

--- build/install.js
+++ build/install.js
@@ -6,17 +6,12 @@ window.addEventListener('beforeinstallprompt', (e) => {
   // Stash the event so it can be triggered later.
   deferredPrompt = e;
   // Update UI to notify the user they can add to home screen
-	if (localStorage.getItem('alertclicks').includes('Install;')){
-  	document.getElementById('a2hsprompt').style.display = 'block';}
-	else {
-		document.getElementById('a2hsprompt').style.display = 'block';
 		addBtn.style.display = 'block';
-	}
 
   addBtn.addEventListener('click', (e) => {
     // hide our user interface that shows our A2HS button
     addBtn.style.display = 'none';
-		document.getElementById('a2hsprompt').style.display = 'none';}
+
     // Show the prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
